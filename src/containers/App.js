30153bd25e2749e8a9e1159aa99464815cc7491d
Jose Bermeo <jbermeo10@gmail.com>
2020-12-11 16:47:39 -0500

    anadiendo archivos del proyecto

---
+++ src/containers/App.js
@@ -0,0 +1,89 @@
+import React, { Component} from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import CardList from '../components/CardList';
+//import { robots } from './robots';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      //robots: robots,
+      robots: [],
+      searchfield: ''
+    }
+    //console.log('constructor');
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response =>response.json())
+      .then(users => this.setState({ robots: users}));
+
+    //Robots hardcoded
+    //this.setState({ robots: robots});
+    //console.log('componentDidMount');
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value})
+
+    //const filteredRobots = this.state.robots.filter(robot =>{
+    //  return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    //})
+    //console.log(filteredRobots);
+  }
+
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+    //console.log('render');
+
+    //if (robots.length === 0) {  //this line equals the following:
+    //if (!robots.length) {
+
+    //here use of ternary for if else sentence
+    return !robots.length ?
+    <h1>Loading...</h1> :
+    (
+      <div className='tc'>
+        <h1 className='f1'>Amigos Robots</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <Scroll>
+          <CardList robots={filteredRobots}/>
+        </Scroll>
+      </div>
+    );
+
+  }
+}
+
+export default App;
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './containers/App';
+//import Hello from './Hello';
+//import Card from './Card';
+//import CardList from './CardList';
+//import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+//import { robots } from './robots';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <React.StrictMode>
+  //    <App />
+  // </React.StrictMode>,
+
+  //<h1>Hola Mundo</h1>,
+
+  //<Hello greeting={'Hello ' + 'React Ninja'}/>,
+  //document.getElementById('root')
+
+  // <div>
+  //   <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+  //   <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+  //   <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+  // </div>
+
+  //<CardList robots={robots}/>
+
+  <App />, document.getElementById('root')
+
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+//reportWebVitals();
